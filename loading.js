84f17555b45ae1093fa853a44f459eb7396ad678
Mads Dyg <168577276+TheRealDoxi@users.noreply.github.com>
2025-04-23 10:30:24 +0200

    Delete loading.js

--- loading.js
+++
@@ -1,7 +0,0 @@
-  // Vent i 3 sekunder og gå derefter videre til næste side
-
-  setTimeout(function () {
-    window.location.href = "index.html"; // Skift til den ønskede HTML-side
-  }, 3000);
-
-  
