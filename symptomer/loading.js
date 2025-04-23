9ae724103d167ff203422821733d0288c0657762
Mads Dyg <168577276+TheRealDoxi@users.noreply.github.com>
2025-04-23 10:39:16 +0200

    Add files via upload

---
+++ symptomer/loading.js
@@ -0,0 +1,7 @@
+  // Vent i 3 sekunder og gå derefter videre til næste side
+
+  setTimeout(function () {
+    window.location.href = "index.html"; // Skift til den ønskede HTML-side
+  }, 3000);
+
+  
